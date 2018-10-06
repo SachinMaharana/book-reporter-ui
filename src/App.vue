d0ee3367d9cbc6c87dd858f7f9be83cca222887f
SachinMaharana <sachin.nicky@gmail.com>
2018-10-06 22:42:48 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header>Hello</Header>
+    <drop-down></drop-down>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import DropDown from './components/DropDown.vue'
+
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header,
+    DropDown
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
+  /* background-color: honeydew; */
+  /* background-image: linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%); */
+  background-image: linear-gradient(to top, #dfe9f3 0%, white 100%);
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+      <h2 id="header-title">{{header}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+        header: ""
+    }
+  } 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  display: grid;
+  width: 100%;
+  height: 90px;
+  /* background-image: linear-gradient(to top, #a3bded 0%, #6991c7 100%); */
+  background-image: linear-gradient(15deg, #13547a 0%, #80d0c7 100%);
+  justify-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12);
+}
+
+#header-title {
+  color: white;
+  margin: auto;
+}
+</style>
